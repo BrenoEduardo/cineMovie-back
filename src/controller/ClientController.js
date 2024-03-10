e126060b646c8a26739800d8f5877e8c242de2b3
BrenoEduardoCM <breno@codesense.com.br>
2024-03-10 18:16:02 -0300

    changes client controller

--- src/controller/ClientController.js
+++ src/controller/ClientController.js
@@ -24,24 +24,86 @@ router.post("/avaliation-movies", async (req, res) => {
     const { idUser, rating, infoMovie } = req.body;
 
     if (!idUser || !rating || !infoMovie) {
-      return res.status(400).json({ error: "Missing information in request body" });
+      return res
+        .status(400)
+        .json({ error: "Missing information in request body" });
     }
-    await AvaliationModel.create(req.body)
+    await AvaliationModel.create(req.body);
 
     const { _id } = infoMovie;
-    const avaliationsForMovie = await AvaliationModel.find({ "infoMovie._id": _id });
+    const avaliationsForMovie = await AvaliationModel.find({
+      "infoMovie._id": _id,
+    });
     let totalRating = 0;
     for (const aval of avaliationsForMovie) {
       totalRating += aval.rating;
     }
 
     const averageRating = totalRating / avaliationsForMovie.length;
-    await MoviesModel.findByIdAndUpdate(
-      _id,
-      { $set: { ratings: averageRating, quantRatings: avaliationsForMovie.length } },
-    );
+    await MoviesModel.findByIdAndUpdate(_id, {
+      $set: {
+        ratings: averageRating,
+        quantRatings: avaliationsForMovie.length,
+      },
+    });
 
-    return res.json({ success: true, message: "Avaliação registrada com sucesso" });
+    return res.json({
+      success: true,
+      message: "Avaliação registrada com sucesso",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+router.post("/filterMovies", async (req, res) => {
+  try {
+    const { typeFilter, filter } = req.body;
+    let query = {};
+    if (typeFilter && filter) {
+      if (Array.isArray(typeFilter)) {
+        query = {
+          $or: typeFilter.map((type, index) => {
+            if (type === "name") {
+              return { name: { $regex: filter, $options: "i" } };
+            } else if (type === "directors") {
+              return {
+                "directors.directorName": {
+                  $regex: filter,
+                  $options: "i",
+                },
+              };
+            } else if (type === "actors") {
+              return {
+                "actors.actorName": { $regex: filter, $options: "i" },
+              };
+            } else if (type === "genres") {
+              return {
+                "genres.genresName": { $regex: filter, $options: "i" },
+              };
+            }
+          }),
+        };
+      } else {
+        if (typeFilter === "name") {
+          query = { name: { $regex: filter, $options: "i" } };
+        } else if (typeFilter === "directors") {
+          query = {
+            "directors.directorName": { $regex: filter, $options: "i" },
+          };
+        } else if (typeFilter === "actors") {
+          query = { "actors.actorName": { $regex: filter, $options: "i" } };
+        } else if (typeFilter === "genres") {
+          query = { "genres.genresName": { $regex: filter, $options: "i" } };
+        }
+      }
+    }
+    const movies = await MoviesModel.find(query);
+
+    return res.json({
+      error: false,
+      data: movies,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
